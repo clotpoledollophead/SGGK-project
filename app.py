9f5b9a24ca0764f2073e69f79cdfdb677671c426
clotpoledollophead <80657950+clotpoledollophead@users.noreply.github.com>
2025-05-14 10:54:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -414,12 +414,11 @@ else:
 
 # Footer
 st.markdown("---")
-# Footer section with license information
 st.markdown(
     """
     <div style="text-align: center; font-size: 12px; color: grey;">
       &copy; Hsin-Ying Lee, 2025. Licensed under the Apache License 2.0.
-      <a href="LICENSE" style="color: grey;">View license</a>
+      <a href="https://github.com/clotpoledollophead/SGGK-project/blob/main/LICENSE" style="color: grey;">View license</a>
     </div><br>
     <div style='text-align: center; color: #666; font-size: 0.875rem;'>
     Middle English Text Word Frequency Analysis | Created with Streamlit and Plotly
